cfa7950f517c9cebfaf8bfe54c91bceb201f5d2e
Md. Tofayel Islam <tofayelislam19986@gmail.com>
2025-02-09 12:47:41 +0600

    project setup

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { Button } from "./components/ui/button";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-center ">text</h1>
+      <Button>Click me</Button>
+    </>
+  );
+}
+
+export default App;
